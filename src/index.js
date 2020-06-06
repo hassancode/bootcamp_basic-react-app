63dee240b1330667beefe5c079afd7255ebcac93
hassancode <syedhassanali85@gmail.com>
2020-06-06 14:18:12 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App n={'asdf asdf'} />
   </React.StrictMode>,
   document.getElementById('root')
 );
